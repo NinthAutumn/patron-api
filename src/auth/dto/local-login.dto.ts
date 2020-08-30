385f72dc06896bde9111b1247ad2a30a977338f6
NinthAutumn <arif.iwamoto@gmail.com>
2020-08-30 15:16:18 +0100

    change

--- src/auth/dto/local-login.dto.ts
+++ src/auth/dto/local-login.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, MinLength } from 'class-validator';
+
 export class LocalLoginDTO {
+  @IsNotEmpty()
   credential: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
